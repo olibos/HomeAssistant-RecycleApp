44ddd19b073b5993db6bc330d47f6d78d380860d
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-01-17 23:23:02 +0100

    Ruff & co linting

--- custom_components/recycle_app/config_flow.py
+++ custom_components/recycle_app/config_flow.py
@@ -1,11 +1,14 @@
+"""RecycleApp ConfigFlow definitions."""
 import logging
-from typing import Any, Dict, Optional
+from typing import Any, Optional
+
+from custom_components.recycle_app.api import FostPlusApi, FostPlusApiException
 import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowError, FlowResult
 from homeassistant.helpers.selector import selector
-from custom_components.recycle_app.api import FostPlusApi, FostPlusApiException
 
 from .const import DEFAULT_DATE_FORMAT, DOMAIN
 
@@ -24,17 +27,25 @@ class RecycleAppConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, *_):
         return await self.async_step_setup()
 
-    async def async_step_setup(self, info: Optional[Dict[str, Any]] = None):
-        errors: Dict[str, str] = {}
+    async def async_step_setup(self, info: Optional[dict[str, Any]] = None):
+        errors: dict[str, str] = {}
         if info is not None:
             try:
                 api = FostPlusApi()
                 language: str = info["language"]
-                zip_code_id, zip_code_name = await self.hass.async_add_executor_job(api.get_zip_code, info["zipCode"], language)
-                street_id, street_name = await self.hass.async_add_executor_job(api.get_street, info["street"], zip_code_id, language)
+                zip_code_id, zip_code_name = await self.hass.async_add_executor_job(
+                    api.get_zip_code, info["zipCode"], language
+                )
+                street_id, street_name = await self.hass.async_add_executor_job(
+                    api.get_street, info["street"], zip_code_id, language
+                )
                 house_umber: int = info["streetNumber"]
-                fractions = await self.hass.async_add_executor_job(api.get_fractions, zip_code_id, street_id, house_umber, language)
-                await self.async_set_unique_id(f"RecycleApp-{zip_code_id}-{street_id}-{house_umber}")
+                fractions = await self.hass.async_add_executor_job(
+                    api.get_fractions, zip_code_id, street_id, house_umber, language
+                )
+                await self.async_set_unique_id(
+                    f"RecycleApp-{zip_code_id}-{street_id}-{house_umber}"
+                )
                 self._abort_if_unique_id_configured()
                 name = f"{house_umber} {street_name}, {zip_code_name}"
                 return self.async_create_entry(
@@ -43,23 +54,20 @@ class RecycleAppConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         "zipCodeId": zip_code_id,
                         "streetId": street_id,
                         "houseNumber": house_umber,
-                        "name": name
+                        "name": name,
                     },
-                    options={
-                        "fractions": fractions,
-                        "language": language
-                    }
+                    options={"fractions": fractions, "language": language},
                 )
 
             except FostPlusApiException as error:
-                errors['base'] = error.code
+                errors["base"] = error.code
 
             except FlowError as flow_error:
                 raise flow_error
 
             except Exception as error:
                 _LOGGER.error(f"Failed to register {error}")
-                errors['base'] = "register_failed"
+                errors["base"] = "register_failed"
 
         return self.async_show_form(
             step_id="setup",
@@ -68,24 +76,26 @@ class RecycleAppConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     vol.Required("zipCode"): int,
                     vol.Required("street"): str,
                     vol.Required("streetNumber"): int,
-                    vol.Required("language", default="fr"): selector({
-                        "select": {
-                            "options": ["fr", "nl", "en", "de"],
-                            "mode": "dropdown"
+                    vol.Required("language", default="fr"): selector(
+                        {
+                            "select": {
+                                "options": ["fr", "nl", "en", "de"],
+                                "mode": "dropdown",
+                            }
                         }
-                    })
-                }),
-            errors=errors
+                    ),
+                }
+            ),
+            errors=errors,
         )
+
 
 class RecycleAppOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
         self.config_entry = config_entry
 
-    async def async_step_init(
-        self, user_input: dict[str, Any] = None
-    ) -> FlowResult:
+    async def async_step_init(self, user_input: dict[str, Any] = None) -> FlowResult:
         """Manage the options."""
         if user_input is not None:
             api = FostPlusApi()
@@ -94,26 +104,39 @@ class RecycleAppOptionsFlowHandler(config_entries.OptionsFlow):
             house_umber = self.config_entry.data.get("houseNumber")
             language = user_input.get("language", "fr")
             date_format = user_input.get("format", DEFAULT_DATE_FORMAT)
-            fractions = await self.hass.async_add_executor_job(api.get_fractions, zip_code_id, street_id, house_umber, language)
+            fractions = await self.hass.async_add_executor_job(
+                api.get_fractions, zip_code_id, street_id, house_umber, language
+            )
             return self.async_create_entry(
                 title="",
                 data={
                     "language": language,
                     "format": date_format,
-                    "fractions": fractions
-                })
+                    "fractions": fractions,
+                },
+            )
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema(
                 {
-                    vol.Required("language", default=self.config_entry.options.get("language", "fr")): selector({
-                        "select": {
-                            "options": ["fr", "nl", "en", "de"],
-                            "mode": "dropdown"
+                    vol.Required(
+                        "language",
+                        default=self.config_entry.options.get("language", "fr"),
+                    ): selector(
+                        {
+                            "select": {
+                                "options": ["fr", "nl", "en", "de"],
+                                "mode": "dropdown",
+                            }
                         }
-                    }),
-                    vol.Required("format", default=self.config_entry.options.get("format", DEFAULT_DATE_FORMAT)): str
+                    ),
+                    vol.Required(
+                        "format",
+                        default=self.config_entry.options.get(
+                            "format", DEFAULT_DATE_FORMAT
+                        ),
+                    ): str,
                 }
             ),
         )
--- custom_components/recycle_app/sensor.py
+++ custom_components/recycle_app/sensor.py
@@ -1,58 +1,66 @@
 """RecycleApp sensor."""
+from datetime import date, datetime, timedelta
 import logging
-from datetime import datetime, timedelta, date
-from typing import Any, Dict, Tuple, final
+from typing import Any, final
 
 from homeassistant import config_entries
-from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
+from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceEntryType
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.event import async_track_time_change
-from homeassistant.helpers.update_coordinator import CoordinatorEntity, DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 from .api import FostPlusApi
 from .const import DEFAULT_DATE_FORMAT, DOMAIN, get_icon
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: config_entries.ConfigEntry, async_add_entities):
-    config: Dict = hass.data[DOMAIN][config_entry.entry_id]
+
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: config_entries.ConfigEntry, async_add_entities
+):
+    config: dict = hass.data[DOMAIN][config_entry.entry_id]
     options = config_entry.options
     api = FostPlusApi()
 
     zip_code_id: str = config["zipCodeId"]
     street_id: str = config["streetId"]
     house_number: int = config["houseNumber"]
-    fractions: Dict[str, Tuple[str, str]] = options.get("fractions")
+    fractions: dict[str, tuple[str, str]] = options.get("fractions")
     language: str = options.get("language", "fr")
     date_format: str = options.get("format", DEFAULT_DATE_FORMAT)
-    _LOGGER.debug(f'zip_code_id: {zip_code_id}')
-    _LOGGER.debug(f'street_id: {street_id}')
-    _LOGGER.debug(f'house_number: {house_number}')
-    _LOGGER.debug(f'fractions: {fractions}')
-    _LOGGER.debug(f'language: {language}')
-    _LOGGER.debug(f'format: {date_format}')
+    _LOGGER.debug(f"zip_code_id: {zip_code_id}")
+    _LOGGER.debug(f"street_id: {street_id}")
+    _LOGGER.debug(f"house_number: {house_number}")
+    _LOGGER.debug(f"fractions: {fractions}")
+    _LOGGER.debug(f"language: {language}")
+    _LOGGER.debug(f"format: {date_format}")
 
     async def async_update_collections():
+        """Fetch data."""
         _LOGGER.debug("Update collections")
-        """Fetch data"""
-        return await hass.async_add_executor_job(api.get_collections, zip_code_id, street_id, house_number)
+        return await hass.async_add_executor_job(
+            api.get_collections, zip_code_id, street_id, house_number
+        )
 
     coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
-        name='RecycleAppGetCollections',
-        update_method=async_update_collections
+        name="RecycleAppGetCollections",
+        update_method=async_update_collections,
     )
 
-    last_refresh = datetime.utcnow()
+    last_refresh = datetime.now()
 
     @callback
     async def async_refresh(now):
         nonlocal last_refresh
-        if (datetime.utcnow() - last_refresh).total_seconds() > 120:
-            last_refresh = datetime.utcnow()
+        if (datetime.now() - last_refresh).total_seconds() > 120:
+            last_refresh = datetime.now()
             _LOGGER.debug(f"async_refresh {unique_id}")
             await coordinator.async_refresh()
 
@@ -66,32 +74,45 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: config_entries.Co
         entry_type=DeviceEntryType.SERVICE,
         identifiers={(DOMAIN, unique_id)},
         name=config.get("name", "Collecte des poubelles"),
-        manufacturer='Fost Plus',
-        model='Recycle!'
+        manufacturer="Fost Plus",
+        model="Recycle!",
     )
 
     if isinstance(fractions, dict):
-        async_add_entities([RecycleAppEntity(
-            coordinator, f"{unique_id}-{fraction}", fraction, color, name, device_info, date_format) for (fraction, (color, name)) in fractions.items()])
+        async_add_entities(
+            [
+                RecycleAppEntity(
+                    coordinator,
+                    f"{unique_id}-{fraction}",
+                    fraction,
+                    color,
+                    name,
+                    device_info,
+                    date_format,
+                )
+                for (fraction, (color, name)) in fractions.items()
+            ]
+        )
     else:
         _LOGGER.error(
-            'Your fractions are in the v1 format... Please delete this address and restart.')
+            "Your fractions are in the v1 format... Please delete this address and restart"
+        )
 
 
 class RecycleAppEntity(CoordinatorEntity, SensorEntity):
     """Base class for all RecycleApp entities."""
 
     def __init__(
-            self,
-            coordinator: DataUpdateCoordinator,
-            unique_id: str,
-            fraction: str,
-            color: str,
-            name: str,
-            device_info: Dict[str, Any] = None,
-            date_format = DEFAULT_DATE_FORMAT
+        self,
+        coordinator: DataUpdateCoordinator,
+        unique_id: str,
+        fraction: str,
+        color: str,
+        name: str,
+        device_info: dict[str, Any] = None,
+        date_format=DEFAULT_DATE_FORMAT,
     ):
-        """Initialize the entity"""
+        """Initialize the entity."""
         super().__init__(coordinator)
         self._unique_id = unique_id
         self._fraction = fraction
@@ -125,14 +146,18 @@ class RecycleAppEntity(CoordinatorEntity, SensorEntity):
     @final
     def state(self) -> str | None:
         value = self.native_value
-        if (value is None):
+        if value is None:
             return None
 
         return value.strftime(self._date_format)
 
     @property
     def native_value(self) -> date | None:
-        return self.coordinator.data[self._fraction] if self._fraction in self.coordinator.data else None
+        return (
+            self.coordinator.data[self._fraction]
+            if self._fraction in self.coordinator.data
+            else None
+        )
 
     @property
     def available(self) -> bool:
@@ -145,7 +170,7 @@ class RecycleAppEntity(CoordinatorEntity, SensorEntity):
     @callback
     def async_write_ha_state(self) -> None:
         value = self.native_value
-        if (value):
+        if value:
             delta: timedelta = value - datetime.now().date()
             self._attr_extra_state_attributes["days"] = delta.days
         else:
