dcd7d4426b20f78ffeee09c5258cbf53a19525bd
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-01-18 23:16:37 +0100

    Start move DataUpdateCoordinator to the init module to avoid code duplication between sensor and calendar.

--- custom_components/recycle_app/__init__.py
+++ custom_components/recycle_app/__init__.py
@@ -1,11 +1,19 @@
 """Custom integration to integrate RecycleApp with Home Assistant."""
+from datetime import datetime
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import Config, HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
+from homeassistant.helpers.event import async_track_time_change
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DOMAIN
+from .api import FostPlusApi
+from .const import DEFAULT_DATE_FORMAT, DOMAIN
 
-PLATFORMS = [Platform.SENSOR]
+PLATFORMS = [Platform.SENSOR, Platform.CALENDAR]
+_LOGGER = logging.getLogger(__name__)
 
 
 async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
@@ -24,6 +32,61 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
 
     hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    config = entry.data
+    options = entry.options
+    api = FostPlusApi()
+
+    zip_code_id: str = config["zipCodeId"]
+    street_id: str = config["streetId"]
+    house_number: int = config["houseNumber"]
+    fractions: dict[str, tuple[str, str]] = options.get("fractions")
+    language: str = options.get("language", "fr")
+    date_format: str = options.get("format", DEFAULT_DATE_FORMAT)
+    _LOGGER.debug(f"zip_code_id: {zip_code_id}")
+    _LOGGER.debug(f"street_id: {street_id}")
+    _LOGGER.debug(f"house_number: {house_number}")
+    _LOGGER.debug(f"fractions: {fractions}")
+    _LOGGER.debug(f"language: {language}")
+    _LOGGER.debug(f"format: {date_format}")
+
+    async def async_update_collections():
+        """Fetch data."""
+        _LOGGER.debug("Update collections")
+        return await hass.async_add_executor_job(
+            api.get_collections, zip_code_id, street_id, house_number
+        )
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="RecycleAppGetCollections",
+        update_method=async_update_collections,
+    )
+
+    last_refresh = datetime.now()
+
+    @callback
+    async def async_refresh(now):
+        nonlocal last_refresh
+        if (datetime.now() - last_refresh).total_seconds() > 120:
+            last_refresh = datetime.now()
+            _LOGGER.debug(f"async_refresh {unique_id}")
+            await coordinator.async_refresh()
+
+    # Fetch initial data so we have data when entities subscribe
+    await coordinator.async_refresh()
+    # Refresh every day at midnight
+    async_track_time_change(hass, async_refresh, hour=0, minute=0, second=0)
+
+    unique_id = f"RecycleApp-{zip_code_id}-{street_id}-{house_number}"
+    device_info = DeviceInfo(
+        entry_type=DeviceEntryType.SERVICE,
+        identifiers={(DOMAIN, unique_id)},
+        name=config.get("name", "Collecte des poubelles"),
+        manufacturer="Fost Plus",
+        model="Recycle!",
+    )
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
