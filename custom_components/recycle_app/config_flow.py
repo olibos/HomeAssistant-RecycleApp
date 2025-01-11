6124fbd6c31fc577d2f4af9a4ea4176deedfe455
Olivier Bossaer <olivier.bossaer@gmail.com>
2025-01-11 16:07:35 +0100

    ✨ Suppress config_entry parameter

--- custom_components/recycle_app/config_flow.py
+++ custom_components/recycle_app/config_flow.py
@@ -31,10 +31,10 @@ class RecycleAppConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(
-        config_entry: config_entries.ConfigEntry,
+        _: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Create the options flow."""
-        return RecycleAppOptionsFlowHandler(config_entry)
+        return RecycleAppOptionsFlowHandler()
 
     async def async_step_user(self, *_) -> FlowResult:
         """Handle the initial step initiated by the user."""
@@ -240,7 +240,7 @@ class OptionalInt(vol.Coerce):
 class RecycleAppOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle the options flow for RecycleApp."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+    def __init__(self) -> None:
         """Initialize options flow."""
         self._parks = None
         self._data = None
